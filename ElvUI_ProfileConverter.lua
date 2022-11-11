91f7def6c55ba32a0a4fd29f267290ac889a3a7d
Paweł Jastrzębski <pawelj@iosphe.re>
2022-11-11 13:34:33 +0100

    Remade GUI

--- ElvUI_ProfileConverter.lua
+++ ElvUI_ProfileConverter.lua
@@ -1,65 +1,52 @@
 local _G = _G
 local _, ns = ...
 
+local LibBase64 = LibStub("LibBase64-1.0-ElvUI")
 local LibDeflate = LibStub("LibDeflate")
 local LibCompress = LibStub("LibCompress")
-local LibBase64 = LibStub("LibBase64-1.0-ElvUI")
-local AceGUI = LibStub("AceGUI-3.0")
+local ElvUIPlugin = LibStub("LibElvUIPlugin-1.0")
 
-ns = LibStub("AceAddon-3.0"):NewAddon("ElvUI Profile Converter", "AceConsole-3.0")
+ns = LibStub("AceAddon-3.0"):NewAddon("ElvUI Profile Converter")
 
 function ns:Convert(dataString)
    if strfind(dataString, '!E1!') then
-      return "Error: Input already uses a new format.", false
+      return "Error: Input already uses a new format."
    end
    if not LibBase64:IsBase64(dataString) then
-      return "Error: Input doesn't look like a correct profile.", false
+      return "Error: Input doesn't look like a correct profile."
    end
    
    local decodedData = LibBase64:Decode(dataString)
    local decompressedData, decompressedMessage = LibCompress:Decompress(decodedData)
    
    if not decompressedData then
-      return format("Error decompressing data: %s.", decompressedMessage), false
+      return format("Error decompressing data: %s.", decompressedMessage)
    end
    
    local compressedData = LibDeflate:CompressDeflate(decompressedData, {level = 5})
    local profileExport = LibDeflate:EncodeForPrint(compressedData)
    
-   return "!E1!"..profileExport, true
-end
-
-function ns:HandleConversion()
-   local payload, sucess = ns:Convert(ns.gui.editbox:GetText())
-   if sucess then
-      ns.gui.editbox:SetText(payload)
-      ns.gui.editbox:HighlightText()
-      ns.gui.frame:SetStatusText("Conversion complete. The profile above can be pasted into ElvUI.")
-   else
-      ns.gui.editbox:SetText("")
-      ns.gui.frame:SetStatusText(payload)
-   end
-end
-
-function ns:SlashCommand()
-	ns.gui.frame:Show()
+   return "!E1!"..profileExport
 end
 
 function ns:OnInitialize()
-   ns.gui = {}
-   ns.gui.frame = AceGUI:Create("Frame")
-   ns.gui.frame:SetTitle("|cff1784d1ElvUI|r Profile Converter")
-   ns.gui.frame:SetLayout("Fill")
-   ns.gui.editbox = AceGUI:Create("MultiLineEditBox")
-   ns.gui.editbox:SetLabel("Paste the old profile into the editbox below:")
-   ns.gui.editbox:SetFullWidth(true)
-   ns.gui.editbox:SetFullHeight(true)
-   ns.gui.editbox:SetCallback("OnEnterPressed", ns.HandleConversion)
-   ns.gui.frame:AddChild(ns.gui.editbox)
-   ns.gui.frame:Hide()
+   ns.status = ""
 
-   _G.ElvUIConverterFrame = ns.gui.frame
-   tinsert(_G.UISpecialFrames, "ElvUIConverterFrame")
+   local myOptionsTable = {
+      type = "group",
+      name = "Profile Converter",
+      args = {
+        convert = {
+          name = "Paste the old profile into the editbox below:",
+          type = "input",
+          width = "full",
+          multiline = 40,
+          set = function(_, val) ns.status = ns:Convert(val) end,
+          get = function(_) return ns.status end
+        }
+      }
+    }
 
-   self:RegisterChatCommand("econvert", "SlashCommand")
+   ElvUIPlugin:RegisterPlugin("ElvUI_ProfileConverter")
+   _G.ElvUI[1].Options.args.profileconverter = myOptionsTable
 end
